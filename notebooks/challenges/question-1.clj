b030747812b1eb2be599523f9e18bc07cfc75561
Ethan Miller <ethanzanemiller@gmail.com>
2025-11-11 22:01:38 -0500

    Simplify question 1 answer

--- notebooks/challenges/question-1.clj
+++ notebooks/challenges/question-1.clj
@@ -1,59 +1,18 @@
 (ns workshop.question-1
-  (:require [tablecloth.api :as tc]
-            [tablecloth.column.api :as tcc]
-            [scicloj.tableplot.v1.plotly :as plotly]
-            [java-time :as jt]))
+  (:require [tablecloth.api :as tc] 
+            [scicloj.tableplot.v1.plotly :as plotly]))
 
 ;; ## 1. What is the geographical distribution of requests? Any concentrations?
-
-(def src-fmt
-  (jt/formatter "yyyy/MM/dd HH:mm:ssX"))
-
-(defn ->first-day-of_month [str]
-  (->> str
-       (jt/local-date src-fmt)
-       (#(jt/adjust % :first-day-of-month))))
-
-(def workshop-data
-  (tc/dataset "data/clt-311-workshop.csv" {:key-fn keyword}))
 
 ;; Let's see what geographic data we have
 (-> workshop-data
     (tc/select-columns [:COUNCIL_DISTRICT :CMPD_DIVISION :NEIGHBORHOOD_PROFILE_AREA :ZIP_CODE])
-    (tc/head 10))
-
-(def top-five-requests
-  (-> workshop-data
-      (tc/group-by [:REQUEST_TYPE])
-      (tc/aggregate {:count tc/row-count})
-      (tc/order-by :count :desc)
-      (tc/head 5)
-      :REQUEST_TYPE
-      set))
-
-(def month-data
-  (-> workshop-data
-      (tc/select-rows
-       (fn [row] (-> row
-                     :REQUEST_TYPE
-                     top-five-requests)))
-      ;; this time we'll use map-columns, another way to add a column
-      (tc/map-columns :MONTH
-                      [:RECEIVED_DATE]
-                      (fn [datestr]
-                        (->> datestr
-                             (jt/local-date src-fmt)
-                             jt/month
-                             jt/value)))
-      (tc/order-by :MONTH)))
-
+    (tc/head))
 
 ;; Zip code could be useful, but the CMPD_DIVISION is more human understandable
 ;; These are the Charlotte Police divisons.
 
-(-> month-data
-    (tc/select-rows
-     (comp #(= % "NON_RECYCLABLE ITEMS") :REQUEST_TYPE))
+(-> workshop-data 
     (tc/group-by [:CMPD_DIVISION])
     (tc/aggregate {:COUNT tc/row-count})
     (tc/order-by :COUNT)
@@ -62,8 +21,19 @@
       :=y :COUNT}))
 
 ;; That's interesting b/c we see a big bump in SOUTH. Looking up South, this is
-;; an area of relative affluence and single-family homes. Could be there's an
-;; association of recycling with affluence and/or home types? Maybe also if
-;; these are homes the acquire more and throw out more? 
+;; an area of relative affluence and single-family homes. What would happen if
+;; we excluded the recyclable items requests?
+
+(-> workshop-data
+    (tc/select-rows
+       (comp #(not= % "NON_RECYCLABLE ITEMS") :REQUEST_TYPE))
+    (tc/group-by [:CMPD_DIVISION])
+    (tc/aggregate {:COUNT tc/row-count})
+    (tc/order-by :COUNT)
+    (plotly/layer-bar
+     {:=x :CMPD_DIVISION
+      :=y :COUNT}))
+
+;; So this concentration in South does seem to come from recyclable items.
 
 
